50f74fabceed85db6db5fff245dee370d05f9226
Dallas Little <mdtdallas@hotmail.com>
2022-06-12 13:36:30 +1000

    new

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const db = require("./db");
 const logModel = require("./models/logModel");
 const secret_key = process.env.SESSION_TOKEN_SECRET;
 
-server.use(cors({ origin: "http://localhost:8080" }));
+server.use(cors({ origin: "http://localhost:3000" }));
 
 // get IPs from the database push to new array
 let validIps = ["::1"]; // Put your IP whitelist in this array
